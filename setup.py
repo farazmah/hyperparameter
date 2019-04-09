586b804c01b8d715be3018e15b16cdac8660feb5
faraz <FMahmoodian@officeworks.com.au>
2019-04-10 09:47:45 +1000

    minor change to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='frameworks_hyperparameter',
+    name='hyperparameter',
     py_modules=['lightgbm_hyper'],
     version='0.1',
     description='Hyperparameter tuning for lightgbm using hyperopt',
-    author='faraz mahmoodian'
+    author='Faraz Mahmoodian'
 )
