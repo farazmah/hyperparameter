b988a6569a3b886b4f67d9b1c919035316c39390
faraz <FMahmoodian@officeworks.com.au>
2019-04-11 15:22:08 +1000

    adding custom cross_val_score to handle groupkfolds

--- hyperparameter/hyperparameter.py
+++ hyperparameter/hyperparameter.py
@@ -8,7 +8,7 @@ from abc import ABCMeta, abstractmethod
 import pandas as pd
 import numpy as np
 
-from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_squared_error, log_loss
 import lightgbm
 
 from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
@@ -35,12 +35,10 @@ class Hyperparemeter:
 
     attributes
     ----------
-    loss_metric: str
-        specifying the loss function to be minimised.  Dependent on is_classifier paramter
-    estimator: lightgbm object
-        lightgbm estimator object. Classifier or Regressor dependent on is_classifier parameter
+    loss_metric: sklearn.metrics object
+        specifying the loss function to be minimised.  Dependent on is_classifier parameter
     params: dict
-        tuned paramters after tune_model method is called
+        tuned parameters after tune_model method is called
     trials: hyperopt.Trials
         the hyperopt trials object after hyperparameter tuning.
     """
@@ -48,19 +46,51 @@ class Hyperparemeter:
     __metaclass__ = ABCMeta
 
     def __init__(self, is_classifier=False):
+        self.is_classier = is_classifier
         if is_classifier:
-            self.loss_metric = 'neg_log_loss'
+            self.loss_metric = log_loss
         else:
-            self.loss_metric = 'neg_mean_squared_error'
+            self.loss_metric = mean_squared_error
         self.params = None
         self.trials = None
 
     @classmethod
     @abstractmethod
-    def optimize(cls, trials, score, evals_rounds):
+    def optimize(cls, trials, score, evals_rounds, mon_cons, categorical):
         raise NotImplementedError
 
-    def create_loss_func(self, x_train, y_train, folds):
+    def cross_validation_score(self, model, x, y, cv, groups):
+        """
+        The purpose of this function is to calculate the crossvalidation accuracy measure
+
+        :param model: xgboost.booster or lightgbm.booster
+            estimator used to fit and predict
+        :param x: numpy.array
+            training dataset
+        :param y: numpy.array
+            training labels
+        :param cv: sklearn.model_selection object
+            sklearn cross validation object used for accuracy measure
+        :param groups: numpy.array
+            Used only for GroupKFolds and GroupShuffleSplit
+        :return: losses: float
+            Average loss for the folds
+        """
+        losses = []
+        for train_idx, test_idx in cv.split(x, y, groups):
+            x_tr, x_te = x[train_idx], x[test_idx]
+            y_tr, y_te = y[train_idx], y[test_idx]
+
+            model.fit(x_tr, y_tr)
+            if self.is_classier:
+                test_preds = model.predict_proba(x_te)[:, 1]
+            else:
+                test_preds = model.predict(x_te)[:, ]
+            loss = self.loss_metric(y_true=y_te, y_pred=test_preds)
+            losses.append(loss)
+        return np.mean(losses)
+
+    def create_loss_func(self, x_train, y_train, folds, groups):
         """
         This function is responsible for creating the score function based on training data and kfolds object
 
@@ -70,21 +100,22 @@ class Hyperparemeter:
             The training label
         :param folds: sklearn.model_selection
             the kfold crossvalidation object
+        :param groups: numpy.array
+            index of groups used for GroupKfold
         :return: loss_func: function
             loss function to be minimised
         """
-
         def loss_func(params):
             logging.info("Training with params : ")
             logging.info(params)
             model = self.estimator.set_params(**params)
-            loss = cross_val_score(model, x_train, y_train, cv=folds, scoring=self.loss_metric).mean() * -1
+            loss = self.cross_validation_score(model, x_train, y_train, folds, groups)
             logging.info("\tLoss {0}\n".format(loss))
             return {'loss': loss, 'status': STATUS_OK}
 
         return loss_func
 
-    def tune_model(self, ds_x, ds_y, folds, eval_rounds=100, trials=None, mon_cons=None, categorical=None):
+    def tune_model(self, ds_x, ds_y, folds, eval_rounds=100, groups=None, trials=None, mon_cons=None, categorical=None):
         """
         Main function responsible for tuning hyperparameters
 
@@ -96,8 +127,14 @@ class Hyperparemeter:
             fkolds for crossvaliation of hyperparameters
         :param eval_rounds: int
             number of iterations to run the hyperparameter turning
+        :param groups numpy.array
+            index of groups used for GroupKFold
         :param trials: hyperopt.Trials object
             pretuned hyperopt trials object if available
+        :param mon_cons: str(tuple) for xgboost, tuple for lightgbm
+            index of monotonic constraints
+        :param categorical: list
+            index of categorical feature for lightgbm
         :return: parameters: dict
             the best hyperparameters
         """
@@ -109,7 +146,7 @@ class Hyperparemeter:
             additional_evals = len(trials.losses()) + eval_rounds
 
         # Create the loss function
-        loss_func = self.create_loss_func(ds_x, ds_y, folds)
+        loss_func = self.create_loss_func(ds_x, ds_y, folds, groups)
 
         # Find optimal hyperparameters
         parameters = self.optimize(trials, loss_func, additional_evals, mon_cons, categorical)
@@ -135,7 +172,7 @@ class LightgbmHyper(Hyperparemeter):
             self.estimator = lightgbm.LGBMRegressor()
 
     @classmethod
-    def optimize(cls, trials, score, evals_rounds, mon_cons=None, categorical=None):
+    def optimize(cls, trials, score, evals_rounds, mon_cons, categorical):
         """
         This function specifies the hyperparameter search space and minimises the score function
 
@@ -145,7 +182,11 @@ class LightgbmHyper(Hyperparemeter):
             the loss or score function to be minimised
         :param evals_rounds: int
             number of evaluation rounds for hyperparameter tuning
-        :return: best: dict
+        :param mon_cons: str(tuple) for xgboost, tuple for lightgbm
+            index of monotonic constraints
+        :param categorical: list
+            index of categorical feature for lightgbm
+        :return best: dict
             the best hyperparameters
         """
         space = {
@@ -160,11 +201,10 @@ class LightgbmHyper(Hyperparemeter):
             'reg_lambda': hp.quniform('reg_lambda', 0.01, 1, 0.01),
             'reg_alpha': hp.quniform('reg_alpha', 1, 10, 0.01),
             'monotone_constraints': mon_cons,
-            'categorical_feature': categorical
+            # 'categorical_feature': categorical
         }
 
         best = fmin(score, space, algo=tpe.suggest, trials=trials, max_evals=evals_rounds)
         logging.info('BEST_PARAMETERS')
         logging.info(best)
         return best
-
